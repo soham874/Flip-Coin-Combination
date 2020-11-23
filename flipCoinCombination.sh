59a0225787b048d4507c513ecd4c3703b822d61e
soham874 <soham874@gmail.com>
2020-11-23 15:08:46 +0530

    Added : Singlet simulation and win percent

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-flip=$(($RANDOM%2))
+declare -A singlet
+headwon=0
+tailwon=0
 
-if [ $flip -eq 0 ]
-then
-	echo "Tail"
-else
-	echo "head"
-fi
+for ((i=1;i<=$number;i++))
+do
+	flip=$(($RANDOM%2))
+	singlet[$i]=$flip
+	if [ $flip -eq 0 ]
+	then
+		((tailwon++))
+	else
+		((headwon++))
+	fi	
+done	
+
+echo "Stored simuation results are : "${singlet[@]}
+echo "Singlet combination head win percent = "
+echo $headwon $number | awk '{print 100*$1/$2}'
+echo "Singlet combination tail win percent = "
+echo $tailwon $number | awk '{print 100*$1/$2}'
